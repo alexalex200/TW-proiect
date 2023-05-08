263a3194869c51e612480dfdef4411a93691a467
Anca Alexandru <alex.anca.alex.2002@gmail.com>
2023-05-08 17:28:23 +0300

    commit

--- laborator9.js
+++ laborator9.js
@@ -16,4 +16,23 @@ window.onload=function(){
     {
         document.getElementById("abc").style.backgroundColor="red";
     }
+
+    document.getElementById("adauga").onclick=function()
+    {
+        var p=document.createElement("p");
+        p.innerHTML="ceva";
+        document.body.appendChild(p);
+        //document.body.insertBefore(p,this);
+        //document.body.appendChild(document.getElementById("de_mutat"));
+    }
+
+    document.getElementById("sterge").onclick=function()
+    {
+        let paragrafe=document.getElementsByTagName("p");
+        if(paragrafe.length)
+        {
+            let ultimul=paragrafe[paragrafe.length-1];
+            ultimul.remove(ultimul);
+        }
+    }
 }
