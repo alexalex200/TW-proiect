78db066e3ee0abbf6d558c2ca4b56cef30f097bb
Anca Alexandru <alex.anca.alex.2002@gmail.com>
2023-05-15 09:13:14 +0300

    commit

--- resurse/js/produse.js
+++ resurse/js/produse.js
@@ -2,79 +2,116 @@ window.onload=function() {
 
 
     document.getElementById("inp-pret").onchange=function(){
-        document.getElementById("infoRange").innerHTML=`(${this.value})`;
+        document.getElementById("infoRange").innerHTML=`(${this.value})`
     }
-    
-    
+
+
     document.getElementById("filtrare").onclick= function(){
         let val_nume=document.getElementById("inp-nume").value.toLowerCase();
+
+        let radiobuttons=document.getElementsByName("gr_rad");
         let val_calorii;
-        let gr_radio=document.getElementsByName("gr_rad");
-        for(let r of gr_radio){
+        for(let r of radiobuttons){
             if(r.checked){
                 val_calorii=r.value;
                 break;
             }
         }
-    
-    
-        let val_pret=document.getElementById("inp-pret").value
-        let val_categ=document.getElementById("inp-categorie").value
-    
-    
+
+        var cal_a, cal_b;
+        if(val_calorii!="toate")
+        {
+            [cal_a, cal_b]=val_calorii.split(":");
+            cal_a=parseInt(cal_a);
+            cal_b=parseInt(cal_b);
+        }
+
+        let val_pret=document.getElementById("inp-pret").value;
+
+        let val_categ=document.getElementById("inp-categorie").value;
+
         var produse=document.getElementsByClassName("produs");
-    
-    
+
         for (let prod of produse){
             prod.style.display="none";
             let nume=prod.getElementsByClassName("val-nume")[0].innerHTML.toLowerCase();
-    
-    
-            let cond1= nume.startsWith(val_nume);
-            let cond2=true
-            if (val_calorii!="toate"){
-                [nra, nrb]=val_calorii.split(":")
-                nra=parseInt(nra);
-                nrb=parseInt(nrb);
-                let calorii=parseInt(prod.getElementsByClassName("val-calorii")[0].innerHTML);
-    
-    
-                cond2= (nra<=calorii && calorii<nrb);
-            }
-    
-    
-            pret=parseInt(prod.getElementsByClassName("val-pret")[0].innerHTML);
-            cond3= (val_pret<=pret);
-    
-    
-            let categorie=prod.getElementsByClassName("val-categorie")[0].innerHTML
-            let cond4 = (val_categ==categorie || val_categ=="toate");
+
+            let cond1= (nume.startsWith(val_nume));
+
+            let calorii=parseInt(prod.getElementsByClassName("val-calorii")[0].innerHTML);
+
+            let cond2= (val_calorii=="toate" || cal_a<=calorii && calorii <cal_b);
+
+            let pret=parseFloat(prod.getElementsByClassName("val-pret")[0].innerHTML);
+
+            let cond3= (pret>=val_pret);
+
+            let categorie=prod.getElementsByClassName("val-categorie")[0].innerHTML;
+            let cond4= ( val_categ=="toate" ||  val_categ==categorie)
+
             if(cond1 && cond2 && cond3 && cond4){
                 prod.style.display="block";
             }
         }
     }
-    
-    
-    
-    
-    
-    
-        document.getElementById("resetare").onclick= function(){
-           
-            document.getElementById("inp-nume").value="";
-           
-            document.getElementById("inp-pret").value=document.getElementById("inp-pret").min;
-            document.getElementById("inp-categorie").value="toate";
-            document.getElementById("i_rad4").checked=true;
-            var produse=document.getElementsByClassName("produs");
-    
-    
-            for (let prod of produse){
-                prod.style.display="block";
-            }
+
+    document.getElementById("resetare").onclick= function(){
+                
+        document.getElementById("inp-nume").value="";
+        
+        document.getElementById("inp-pret").value=document.getElementById("inp-pret").min;
+        document.getElementById("inp-categorie").value="toate";
+        document.getElementById("i_rad4").checked=true;
+        var produse=document.getElementsByClassName("produs");
+        document.getElementById("infoRange").innerHTML="(0)";
+        for (let prod of produse){
+            prod.style.display="block";
         }
-    
-    
     }
-    
+    function sortare (semn){
+        var produse=document.getElementsByClassName("produs");
+        var v_produse= Array.from(produse);
+        v_produse.sort(function (a,b){
+            let pret_a=parseFloat(a.getElementsByClassName("val-pret")[0].innerHTML);
+            let pret_b=parseFloat(b.getElementsByClassName("val-pret")[0].innerHTML);
+            if(pret_a==pret_b){
+                let nume_a=a.getElementsByClassName("val-nume")[0].innerHTML;
+                let nume_b=b.getElementsByClassName("val-nume")[0].innerHTML;
+                return semn*nume_a.localeCompare(nume_b);
+            }
+            return semn*(pret_a-pret_b);
+        });
+        for(let prod of v_produse){
+            prod.parentElement.appendChild(prod);
+        }
+    }
+    document.getElementById("sortCrescNume").onclick=function(){
+        sortare(1);
+    }
+    document.getElementById("sortDescrescNume").onclick=function(){
+        sortare(-1);
+    }
+
+    window.onkeydown=function(e)
+    {
+        if(e.key=="c" && e.altKey)
+        {
+            console.log("asd");
+            if(document.getElementById("info-suma"))
+                return;
+            var produse=document.getElementsByClassName("produs");
+            let suma=0;
+            for (let prod of produse){
+                let pret=parseFloat(prod.getElementsByClassName("val-pret")[0].innerHTML);
+                suma+=pret;
+            }
+            let p=document.createElement("p");
+            p.innerHTML=suma;
+            p.id="info-suma"
+            ps=documet.getElementById("p-suma");
+            container=ps.parentNode;
+            frate=ps.nextElementSibling;
+            container.insertBefore(p,frate);
+        }
+    }
+}
